24654eb319d58ebf0a410db49fac93d67ba5fd35
VISHAL RAJ <87539634+VR903@users.noreply.github.com>
2022-12-14 15:26:17 +0530

    Update meeting_option.dart

--- lib/widgets/meeting_option.dart
+++ lib/widgets/meeting_option.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zoom_clone_tutorial/utils/colors.dart';
+import 'package:meet/utils/colors.dart';
 
 class MeetingOption extends StatelessWidget {
   final String text;
